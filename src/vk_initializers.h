420d9f4aebc9cc63f444718d46bd2b63f903e53f
Aureliano <awsidc@gmail.com>
2022-01-03 17:57:13 -0800

    Src changes

---
+++ src/vk_initializers.h
@@ -0,0 +1,11 @@
+// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+namespace vkinit {
+	//vulkan init code goes here
+}
+
