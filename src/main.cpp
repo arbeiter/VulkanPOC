f1222b77401fa8604dbfaae9ca5b6c910329b6a7
Aureliano <awsidc@gmail.com>
2022-01-14 17:46:39 -0800

    Overwrite everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-#include "vk_engine.h"
+#include <vk_engine.h>
 
 int main(int argc, char* argv[])
 {
 	VulkanEngine engine;
-	engine.init();
+
+	engine.init();	
+	
 	engine.run();	
+
 	engine.cleanup();	
+
 	return 0;
 }
